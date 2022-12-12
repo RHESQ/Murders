474dd6b780dfbe0d8d0c39872a3c19eb8c225c38
selorm <lumorselorm85@gmail.com>
2022-12-12 12:16:17 +1200

    visualized data

--- Exploring_Mur.R
+++ Exploring_Mur.R
@@ -12,6 +12,7 @@ setwd("C:/Users/KELVIN/Desktop/Analytics/Murders")
 library(dslabs)
 library(tidyverse)
 library(statip)
+library(ggthemes)
 
 data("murders")
 
@@ -47,4 +48,73 @@ murders %>%
             iqr = IQR(total)) %>% 
   arrange(Average) %>% 
   view()
+
+## 1. Data component
+      
+## 2. Geometric component
+      #boxplot, scatter plot
+
+## 3. aesthetics mapping
+      # 
+
+## 4. scale component
+      #log scale
+
+## 5. Labeling, Title, Legend
+
+
+
+
+rate <- murders %>% 
+  sumarise(rate = sum(total) / sum(poplation) * 10^6) %>% 
+  .$rate
+
+
+region_rate <- murders %>% 
+  select(everything()) %>% 
+  group_by(region) %>% 
+  summarise(region_rate = sum(total) / sum(population) * 10^6) %>% 
+  .$region_rate
+
+region_rate
+
+S_r <- murders %>% 
+  select(everything()) %>% 
+  filter(region %in% "South") %>% 
+  summarise(S_r = sum(total) / sum(population) * 10^6) %>% 
+  .$S_r
+S_r
+
+NC_r <- murders %>% 
+  select(everything()) %>% 
+  filter(region %in% "North Central") 
+  summarise(NC_r = sum(total) / sum(population) * 10^6) %>% 
+  .$Nc_r
+NC_r
+
+W_r <- murders %>% 
+  select(everything()) %>% 
+  filter(region %in% "West") %>% 
+  summarise(W_r = sum(total) / sum(population) * 10^6) %>% 
+  .$W_r
+W_r
+
+murders %>% 
+  ggplot(aes(region, total, col = region)) +
+  geom_boxplot() +
   
+  geom_hline(yintercept = (region_rate), 
+             linetype = "dashed", color = "darkgrey") +
+  
+  
+  scale_y_log10() +
+  theme(
+    panel.grid = element_blank(),
+    plot.title = element_text(hjust = 0.5)
+    ) +
+  xlab("Total count of Murder") +
+  ylab("Region of States") +
+  ggtitle("Distribution of Murders in the U. S") +
+  theme_economist()
+
+
